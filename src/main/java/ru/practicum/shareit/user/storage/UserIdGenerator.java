135546d4006ebe9194bd45344a87bc32b662df10
Mark <igushkin.m@yandex.ru>
2023-06-14 13:45:34 +0100

    style

--- src/main/java/ru/practicum/shareit/user/storage/UserIdGenerator.java
+++ src/main/java/ru/practicum/shareit/user/storage/UserIdGenerator.java
@@ -2,7 +2,7 @@ package ru.practicum.shareit.user.storage;
 
 public class UserIdGenerator {
     private static Integer id = 1;
-
+    
     public static Integer getId() {
         return id++;
     }
